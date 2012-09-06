2d0b4f0bc98d826edfb67dc5c0acd6357083b4c1
dukai <dukai86@gmail.com>
2012-09-06 17:33:23 +0800

    common change

--- resource/js/game.js
+++ resource/js/game.js
@@ -114,118 +114,21 @@ MainApp.addEventListener(bottomHandler, 'mouseout', function(e){
 		MainApp.viewport._move.keep = false;
 		VIEWPORT_LOCK.BOTTOM = false;
 	}
-	//MainApp.viewport.move(new Vector(0, 0), 0);
-});
-/*
-var LOCK = {
-	UP: false,
-	DOWN: false,
-	LEFT: false, 
-	RIGHT: false
-}
-MainApp.addEventListener(soldier, 'keydown', function(e){
-	var KEY = MainApp.INPUT.KEY;
-	switch(e.which){
-		case KEY.UP:
-			if(!LOCK.UP){
-				this.speed.add(new Vector(0, -80));
-				LOCK.UP = true;
-			}
-			break;
-		case KEY.DOWN:
-			if(!LOCK.DOWN){
-				this.speed.add(new Vector(0, 80));
-				LOCK.DOWN = true;
-			}
-			break;
-		case KEY.LEFT:
-			if(!LOCK.LEFT){
-				this.speed.add(new Vector(-80, 0));
-				LOCK.LEFT = true;
-			}
-			break;
-		case KEY.RIGHT:
-			if(!LOCK.RIGHT){
-				this.speed.add(new Vector(80, 0));
-				LOCK.RIGHT = true;
-			}
-			break;
-	}
 });
 
-MainApp.addEventListener(soldier, 'keyup', function(e){
-	var KEY = MainApp.INPUT.KEY;
-	switch(e.which){
-		case KEY.UP:
-			if(LOCK.UP){
-				this.speed.remove(new Vector(0, -80));
-				LOCK.UP = false;
-			}
-			break;
-		case KEY.DOWN:
-			if(LOCK.DOWN){
-				this.speed.remove(new Vector(0, 80));
-				LOCK.DOWN = false;
-			}
-			break;
-		case KEY.LEFT:
-			if(LOCK.LEFT){
-				this.speed.remove(new Vector(-80, 0));
-				LOCK.LEFT = false;
-			}
-			break;
-		case KEY.RIGHT:
-			if(LOCK.RIGHT){
-				this.speed.remove(new Vector(80, 0));
-				LOCK.RIGHT = false;
-			}
-			break;
-	}
-});
-*/
 var sence1Map = new TiledMap(xml, MainApp.viewport);
+
+
 
 MainApp.addEventListener(MainApp.viewport, 'click', function(e){
 	//this.shake();
 });
 
-MainApp.addEventListener(MainApp.viewport, 'mousemove', function(e){
-/*	
-	var pos = new Vector(e.relX, e.relY);
-	
-	var leftRect = new Rect(new Vector(0, 0), 40, 480);
-	var rightRect = new Rect(new Vector(600, 0), 40, 480);
-	var topRect = new Rect(new Vector(0, 0), 640, 40);
-	var bottomRect = new Rect(new Vector(440, 0), 640, 40);
-	
-	if(leftRect.checkContain(pos)){
-		this.moveRight();
-	}
-	
-	
-
-	if(e.relX < 40){
-		this.move(new Vector(120, 0), 10);
-	}else if(e.relX > 600){
-		this.move(new Vector(-120, 0), 10);
-	}else{
-		this.move(new Vector(0, 0), 0);
-	}
-
-	if(e.relY < 20){
-		this.move(new Vector(0, 20), 10);
-	}else if(e.relY > 460){
-		this.move(new Vector(0, -20), 10);
-	}else{
-		this.move(new Vector(0, 0), 0);
-	}
-*/
-});
+MainApp.addEventListener(MainApp.viewport, 'mousemove', function(e){});
 
 
 
 MainApp.ScreenObjPool.add(sence1Map);
-//MainApp.ScreenObjPool.add(soldier);
 MainApp.ScreenObjPool.add(leftHandler);
 MainApp.ScreenObjPool.add(rightHandler);
 MainApp.ScreenObjPool.add(topHandler);

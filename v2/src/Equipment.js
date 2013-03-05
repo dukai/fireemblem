b18e4dc2b0490ca21ab0ced68db3296f55d72c3e
DK <dukai86@gmail.com>
2013-03-06 00:04:41 +0800

    update equipment

--- v2/src/Equipment.js
+++ v2/src/Equipment.js
@@ -11,9 +11,44 @@ var Equipment = function(){
 	this.mobility;
 };
 
+Equipment.type = {
+	
+}
+
 /**
  *装备管理器
  */
 var EquipmentsManager = function(){
+	this.equipments = {
+		//头部
+		head: null,
+		//胸部
+		chest: null,
+		//腿部
+		leg: null,
+		//手部
+		hand: null,
+		//手腕
+		wrist: null,
+		//鞋子
+		foot: null,
+		//腰部
+		waist: null,
+		//饰品
+		adornment: {
+			left: null,
+			right: null
+		},
+		//戒指
+		finger: {
+			left: null,
+			right: null
+		}
+	};
 	
+};
+EquipmentsManager.prototype = {
+	equip: function(type, equipment){
+		
+	}
 };

f22f96a61a0bbc30585bddc985de238e8affff28
DK <dukai86@gmail.com>
2013-05-15 02:13:23 +0800

    add enemyCoordinate judge for getRange
    add getAttackRange method

--- models/abstract-model.js
+++ models/abstract-model.js
@@ -16,8 +16,9 @@ AbstractModel.prototype = {
 	 * @param {Object} target {x, y}
 	 * @param {int} range 移动范围
 	 * @param {HitMap} hitmap 碰撞地图 
+	 * @param {JSON} enemyCoordinates 敌人位置
 	 */
-	getRange: function(target, range, hitmap){
+	getRange: function(target, range, hitmap, enemyCoordinates){
 		var preColumn = target.x - range;
 		var nextColumn = target.x + range;
 		var preRow = target.y - range;
@@ -31,7 +32,38 @@ AbstractModel.prototype = {
 				if(dx + dy <= range){
 					
 					if(x !== target.x || y !== target.y){
-						if(hitmap.getPassable(x, y)){
+						if(hitmap.getPassable(x, y) && !enemyCoordinates[x.toString() + y.toString()]){
+							list.push({x: x, y: y});
+						}
+					}
+				}
+			}
+		}
+		return list;
+	},
+	
+	/**
+	 * 获取攻击范围
+	 * @param {Object} target {x, y}
+	 * @param {int} range 范围
+	 * @param {HitMap} hitmap 碰撞地图 
+	 * @param {JSON} enemyCoordinates 敌人位置
+	 */
+	getAttackRange: function(target, range, hitmap, enemyCoordinates){
+		var preColumn = target.x - range;
+		var nextColumn = target.x + range;
+		var preRow = target.y - range;
+		var nextRow = target.y + range;
+		var list = [];
+		for(var x = preColumn; x <= nextColumn; x++){
+			for(var y = preRow; y <= nextRow; y++){
+				var dx = Math.abs(x - target.x);
+				var dy = Math.abs(y - target.y);
+				
+				if(dx + dy <= range){
+					
+					if(x !== target.x || y !== target.y){
+						if(hitmap.getPassable(x, y) && enemyCoordinates[x.toString() + y.toString()]){
 							list.push({x: x, y: y});
 						}
 					}

6a59f08da9a2e53cf042235a071af3140b0edd7e
DK <dukai86@gmail.com>
2013-05-17 02:41:45 +0800

    add image button

--- views/btn-view.js
+++ views/btn-view.js
@@ -5,39 +5,67 @@ var BtnView = function(options){
 BtnView.prototype = {
 	_initBtnView: function(options){
 		Kinetic.Group.call(this, options);
-		var menuBg = new Kinetic.Rect({
-			x: 0,
-			y: 0,
-			width: 140,
-			height:50,
-			fill: 'green'
-		});
+		if(this.getImage()){
+			var menuBg = new Kinetic.Image({
+				x: 0,
+				y: 0,
+				width: this.getWidth(),
+				height: this.getHeight(),
+				fillPatternImage: this.getImage(),
+				fillPatternOffset: {x: 0, y: 0},
+			});
+			menuBg.on('mousedown', function(e){
+				this.setFillPatternOffset(0, 48);
+				this.getLayer().draw();
+				console.log('mousedown');
+			});
+			menuBg.on('mouseup', function(e){
+				this.setFillPatternOffset(0, 0);
+				console.log('mouseup');
+			});
+			this.add(menuBg);
+		}else{
+			var menuBg = new Kinetic.Rect({
+				x: 0,
+				y: 0,
+				width: 140,
+				height:50,
+				fill: 'green'
+			});
+			menuBg.on('mouseover', function(e){
+				this.setFill( '#090');
+				this.getLayer().draw();
+			});
+			menuBg.on('mouseout', function(e){
+				this.setFill('green');
+				this.getLayer().draw();
+			});
 		
-		var word = new Kinetic.Text({
-			x: 0,
-			y: 14,
-			text : this.getText(),
-			fontSize: 24,
-			fontFamily: "Microsoft YaHei",
-			fill: '#fff',
-			height:40,
-			width:140,
-			align : 'center',
-			listening : false
-		});
+			this.add(menuBg);
+		}
 		
-		menuBg.on('mouseover', function(e){
-			this.setFill( '#090');
-			this.getLayer().draw();
-		});
-		menuBg.on('mouseout', function(e){
-			this.setFill('green');
-			this.getLayer().draw();
-		});
+		if(this.getText()){
+			var word = new Kinetic.Text({
+				x: 0,
+				y: 14,
+				text : this.getText(),
+				fontSize: 24,
+				fontFamily: "Microsoft YaHei",
+				fill: '#fff',
+				height:40,
+				width:140,
+				align : 'center',
+				listening : false
+			});
+			this.add(word);
+		}
 		
-		this.add(menuBg);
-		this.add(word);
+		
+		
+		
+		
 	}
 }
 Kinetic.Global.extend(BtnView, Kinetic.Group);
 Kinetic.Node.addGetterSetter(BtnView, 'text');
+Kinetic.Node.addGetterSetter(BtnView, 'image');

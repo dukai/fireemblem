465cdc8d8ea988024538c5c547812cbd747c14a4
DK <dukai86@gmail.com>
2013-08-15 00:12:56 +0800

    完善信息板类

--- views/info-board-view.js
+++ views/info-board-view.js
@@ -13,7 +13,7 @@ InfoBoardView.prototype = {
 			image: resourceLoader.get('info_bg')
 		});
 		
-		var username = new Kinetic.Text({
+		var username = this.username = new Kinetic.Text({
 			x: 0,
 			y: 10,
 			text : '小明',
@@ -50,19 +50,19 @@ InfoBoardView.prototype = {
 			listening : false
 		});
 		
-		var hpPBV = new ProgressBarView({
+		var hpPBV = this.hpPBV = new ProgressBarView({
 			x: 40,
 			y: 30,
 			fill: '#fff'
 		});
 		
-		var expPBV = new ProgressBarView({
+		var expPBV = this.expPBV = new ProgressBarView({
 			x: 40,
 			y: 50,
 			fill: '#fff'
 		});
 		
-		var hpDetail = new Kinetic.Text({
+		var hpDetail = this.hpDetail = new Kinetic.Text({
 			x: 0,
 			y: 20,
 			text : '480/1000',
@@ -75,7 +75,7 @@ InfoBoardView.prototype = {
 			listening : false
 		});
 		
-		var expDetail = new Kinetic.Text({
+		var expDetail = this.expDetail = new Kinetic.Text({
 			x: 0,
 			y: 40,
 			text : '480/1000',
@@ -88,7 +88,7 @@ InfoBoardView.prototype = {
 			listening : false
 		});
 		
-		var attact = new Kinetic.Text({
+		var attack = this.attack = new Kinetic.Text({
 			x: 15,
 			y: 65,
 			text : '攻击 100',
@@ -101,7 +101,7 @@ InfoBoardView.prototype = {
 			listening : false
 		});
 		
-		var armor = new Kinetic.Text({
+		var armor = this.armor = new Kinetic.Text({
 			x: 80,
 			y: 65,
 			text : '护甲 100',
@@ -122,8 +122,47 @@ InfoBoardView.prototype = {
 		this.add(expPBV);
 		this.add(hpDetail);
 		this.add(expDetail);
-		this.add(attact);
+		this.add(attack);
 		this.add(armor);
+	},
+	/**
+	 *设置角色名字  
+ 	 * @param {String} name
+	 */
+	setName : function(name){
+		this.username.setText(name);
+	},
+	/**
+	 *设置当前血量 
+	 * @param {Number} current 当前血量
+	 * @param {Number} total 总血量
+	 */
+	setHP: function(current, total){
+		this.hpDetail.setText(current + '/' + total);
+		this.hpPBV.setPercent(current / total);
+	},
+	/**
+	 * 设置当前经验条
+	 * @param {Number} current
+	 * @param {Number} total
+	 */
+	setEXP: function(current, total){
+		this.expDetail.setText(current + '/' + total);
+		this.expPBV.setPercent(current / total);
+	},
+	/**
+	 *设置攻击 
+ 	 * @param {Number} attack
+	 */
+	setAttact: function(attack){
+		this.attack.setText('攻击 ' + attack);
+	},
+	/**
+	 *设置当前护甲 
+ 	 * @param {Number} armor
+	 */
+	setArmor: function(armor){
+		this.armor.setText('护甲 ' + armor);
 	}
 };
 
